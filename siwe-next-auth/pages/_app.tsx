91b890958557a18d1d7a28821c9501ec4911d395
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-01-05 11:43:47 +0530

    Integration with Next Auth triedf

---
+++ siwe-next-auth/pages/_app.tsx
@@ -0,0 +1,44 @@
+import { SessionProvider } from "next-auth/react";
+import "./styles.css";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
